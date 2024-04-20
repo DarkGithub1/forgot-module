0377c430faae9115e0b4005a4cdb513d93cdbf61
darkgithub1 <darkgithub16457@gmail.com>
2024-04-20 15:20:24 +0530

    minor

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ dotenv.config()
 const app=express();
 app.use(cookieParser())
 app.use(cors({
-    origin:["http://localhost:5174"],
+    origin:["http://localhost:5173"],
     credentials:true
 }))
 app.use(express.json())
